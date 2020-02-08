b68445dcd0734a269b3c56c8aff0fc1ee6df8e94
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-02-08 08:37:53 -0800

    4_5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,19 @@
 package com.company;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person sara = new Person("Sara ", 4, Nationality.NORWEGIAN);
-        Person victor = new Person("Victor ", 40, Nationality.SERBIAN);
-        Person eva = new Person("Eva ", 42, Nationality.NORWEGIAN);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(i);
+        }
 
-        List<Person> people = new ArrayList<>();
-        people.add(sara);
-        people.add(victor);
-        people.add(eva);
-
-        String collect = people.stream()
-                .map(Person::getName)
-                .collect(Collectors.joining(","));
-        
-        System.out.println("Names: " + collect);
+        String sum = numbers.stream()
+                .reduce((Integer::sum))
+                .toString();
+        System.out.println(sum);
     }
 }

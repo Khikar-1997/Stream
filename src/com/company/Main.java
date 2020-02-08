d1b95eac4b7c4bbb99650b507aa6d86ab0afd940
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-02-08 07:51:20 -0800

    4_7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,20 +9,24 @@ public class Main {
         Person sara = new Person("Sara ", 4);
         Person victor = new Person("Victor ", 40);
         Person eva = new Person("Eva ", 42);
-        Person anna = new Person("Anna ", 5);
-        Person mike = new Person("Mike ", 57);
+
         List<Person> people = new ArrayList<>();
         people.add(sara);
         people.add(victor);
         people.add(eva);
-        people.add(anna);
-        people.add(mike);
-        List<String> collect = people.stream()
+
+        List<Person> kids = people.stream()
                 .filter((each) -> each.getAge() < 18)
-                .map(Person::getName)
                 .collect(Collectors.toList());
-        System.out.print("Name of kids - ");
-        collect.forEach(System.out::print);
-        System.out.print(".");
+
+        List<Person> adult = people.stream()
+                .filter((each) -> each.getAge() >= 18)
+                .collect(Collectors.toList());
+
+        Map<Boolean, List<Person>> booleanListMap = new HashMap<>();
+        booleanListMap.put(true, adult);
+        booleanListMap.put(false, kids);
+        booleanListMap.values().forEach(System.out::println);
+
     }
 }
